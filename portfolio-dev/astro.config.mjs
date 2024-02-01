8bf810b57d8336d0c0ec90655e58c6a56f0096a0
Daniel Velerdas Sedano <daniel01velerdas@gmail.com>
2024-02-01 15:59:52 +0100

    Update astro.config.mjs

--- portfolio-dev/astro.config.mjs
+++ portfolio-dev/astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://daniveler.github.io',
-  base: '/portfolio-dev'
+  site: 'https://daniveler.github.io'
 });
