dfd43a5eeb209fcc9e0c42d288d1cdf5223b62e3
daniveler <daniel01velerdas@gmail.com>
2024-02-01 17:59:04 +0100

    Updated astro config

--- portfolio-dev/astro.config.mjs
+++ portfolio-dev/astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://daniveler.github.io',
-  base: 'portfolio-dev'
+  base: '/professional-portfolio'
 });
