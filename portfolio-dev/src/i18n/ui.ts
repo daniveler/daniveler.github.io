9a71b0bd7e0b1076744f7bebe75b5a5dad4ea011
daniveler <daniel01velerdas@gmail.com>
2024-04-20 21:29:21 +0200

    Add Spanish text

--- portfolio-dev/src/i18n/ui.ts
+++ portfolio-dev/src/i18n/ui.ts
@@ -31,7 +31,7 @@ export const ui = {
     - Utilization of API clients such as Postman, Advanced Rest Client, or Insomnia.`,
 
     'experience.telefonica.title': 'API OB Integration Engineer in Telefónica',
-    'experience.telefonica.date': 'January 2022 - Present',
+    'experience.telefonica.date': 'January 2023 - Present',
     'experience.telefonica.text': `- Project integration between Telefónica Kernel Global and Vivo Brazil team.
     - Involvement in the analysis and design processes of 4P APIs.
     - Significant contributions to different phases of the API lifecycle.
@@ -42,14 +42,16 @@ export const ui = {
     'experience.pixel.date': 'February 2022 - May 2022',
     'experience.pixel.text': `- App Development on Android and iOS.
     - Focus on implementing key functions, such as reading and interpreting QR codes.
-    - Efficient data management between applications.
+    - Efficient data management among applications.
     - Integration with a WebService.`,
+
+    'projects.title': 'Projects',
     
     'projects.to-do.title': 'To-Do Web Application',
     'projects.to-do.text': 'The idea of this project was making a simple web application using React JS to get familiar with this framework. This app consists in a list of To-do tasks, with a title and status. All tasks created will be displayed at the main page, from where you can create new tasks, update and delete existing ones.',
 
     'projects.axpe-employees-api.title': 'Axpe Employees API',
-    'projects.axpe-employees-api.text': 'Final exercise from a training period on apification. Rest API service for controlling employees of a company, including its OpenAPI 3.0 specification and implementation in Spring Boot.',
+    'projects.axpe-employees-api.text': 'Final exercise from a training period on apification. Rest API service for controlling employees of a company. It includes its OpenAPI 3.0 specification and implementation in Spring Boot.',
 
     'projects.moviefav.title': 'MovieFav. Movie List App',
     'projects.moviefav.text': 'Mobile Application developed for a technical test, using the Retrofit and Room libraries to read data from an API and to manage a local DB that allows data persistence.',
@@ -69,7 +71,53 @@ export const ui = {
     'nav.language': 'Cambiar Idioma',
     'nav.download-cv': 'Descargar CV',
 
+    'home.name': 'Soy Daniel Velerdas',
+    'home.text': 'API Engineer, Desarrollador Full-Stack y Móvil',
+
     'about.title': 'Sobre mí',
+
+    'experience.title': 'Experiencia Profesional',
+
+    'experience.uxcale.title': 'API Engineer en uXcale',
+    'experience.uxcale.date': 'Septiembre de 2022 - Actualmente',
+    'experience.uxcale.text': `- Desarrollo de programa formativo de introducción a las APIs.
+    - Diseño y especificación de APIs de carácter interno destinadas a la gestión de empleados y actividades.
+    - Documentación acerca de numerosos conceptos API, así como de numerosas tecnologías, como OpenAPI, Apigee, Mulesoft, gRPC, AsyncAPI, GraphQL entre otras.
+    - Implementación de APIs utilizando diferentes entornos (SpringBoot, NodeJS...).
+    - Realización de pruebas y testing con Jest, Supertest, Junit 5 y Mockito.
+    - Utilización de clientes API como Postman, Advanced Rest Client o Insomnia`,
+    
+    'experience.telefonica.title': 'API OB Integration Engineer en Telefónica',
+    'experience.telefonica.date': 'Enero de 2023 - Actualmente',
+    'experience.telefonica.text': `- Integración de proyectos entre Telefónica Kernel Global y la OB de Brasil y equipos de Vivo.
+    - Implicación en los procesos de análisis y diseño de las APIs de 4P.​
+    - Aportación de mejoras significativas en las diferentes fases del ciclo de vida de las APIs.​
+    - Desarrollo y despliegue de numerosas herramientas de integración usando NodeJS, ReactJS y Express.
+    - Documentación formal de todos los procesos llevados a cabo, además de generación de casos de prueba.`,
+
+    'experience.pixel.title': 'Desarrollador Móvil en Pixel Innova',
+    'experience.pixel.date': 'Febrero de 2022 - Mayo de 2022',
+    'experience.pixel.text': `- Desarrollo de aplicaciones en Android e iOS.
+    - Enfoque en la implementación de funciones clave, como la lectura e interpretación de códigos QR.
+    - Gestión eficiente de datos entre las aplicaciones.
+    - Integración con un WebService.`,
+
+    'projects.title': 'Proyectos',
+
+    'projects.to-do.title': 'Aplicación Web To-Do',
+    'projects.to-do.text': 'La idea de este proyecto era crear una aplicación web simple utilizando React JS para familiarizarse con este framework. Esta aplicación consiste en una lista de tareas pendientes, con un título y estado. Todas las tareas creadas se mostrarán en la página principal, desde donde puedes crear nuevas tareas, actualizar y eliminar las existentes.',
+
+    'projects.axpe-employees-api.title': 'API de Empleados de Axpe',
+    'projects.axpe-employees-api.text': 'Ejercicio final de un período de formación en apiificación. Servicio de API REST para gestionar los empleados de una empresa. Incluye la especificación OpenAPI 3.0 y su implementación en Spring Boot.',
+
+    'projects.moviefav.title': 'MovieFav. Aplicación de Listado de Películas',
+    'projects.moviefav.text': 'Aplicación móvil desarrollada para una prueba técnica, utilizando las bibliotecas Retrofit y Room para leer datos de una API y gestionar una base de datos local que permite la persistencia de datos.',
+
+    'projects.survy.title': 'Survy. Aplicación educativa de encuestas y preguntas online',
+    'projects.survy.text': 'Proyecto de Fin de Grado en Ingeniería Informática. Aplicación móvil para gestionar encuestas y preguntas online en el ámbito educativo.', 
+    
+    'projects.weather-vue.title': 'Weather Vue. Aplicación Web del Tiempo',
+    'projects.weather-vue.text': 'Aplicación web simple desarrollada en VueJS para verificar el tiempo actual en cualquier ciudad, alimentada por la API de openweathermap.',
 
   },
 } as const;
