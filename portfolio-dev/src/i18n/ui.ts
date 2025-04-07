dc2a79a95dff8875e515712c43db9aa4a553d551
daniveler <daniel.velerdas@nter.es>
2025-04-07 17:38:25 +0200

    Add NFQ experience

--- portfolio-dev/src/i18n/ui.ts
+++ portfolio-dev/src/i18n/ui.ts
@@ -23,8 +23,20 @@ export const ui = {
     'social-media.title': 'Social Media',
 
     'experience.title': 'Professional Experience',
+
+    'experience.nfq.title': 'SCIB Integration - Financial Crime Project - Banco Santander',
+    'experience.nfq.date': 'October 2024 - Present',
+    'experience.nfq.text': `- Actualización y mantenimiento de servicios.
+    - Documentación técnica de procesos.
+    - Extracción de evidencias sobre cambios e implementaciones en el software.
+    - Desarrollo de herramientas de automatización de tareas.
+    - Gestión de colecciones y testing de APIs en Postman.
+    - Gestión de Bases de Datos.
+    - Implementación de protocolos de seguridad.
+    - Expedición, firma, verificación y gestión de certificados digitales.`,
+
     'experience.uxcale.title': 'API Engineer in uXcale',
-    'experience.uxcale.date': 'September 2022 - Present',
+    'experience.uxcale.date': 'September 2022 - October 2024',
     'experience.uxcale.text': `- Development of an introductory training program for APIs.
     - Design and specification of internal APIs for employee and activity management.
     - Documentation on numerous API concepts and various technologies, such as OpenAPI, Apigee, Mulesoft, gRPC, AsyncAPI, and GraphQL, among others.
@@ -33,7 +45,7 @@ export const ui = {
     - Utilization of API clients such as Postman, Advanced Rest Client, or Insomnia.`,
 
     'experience.telefonica.title': 'API OB Integration Engineer in Telefónica',
-    'experience.telefonica.date': 'January 2023 - Present',
+    'experience.telefonica.date': 'January 2023 - October 2024',
     'experience.telefonica.text': `- Project integration between Telefónica Kernel Global and Vivo Brazil team.
     - Involvement in the analysis and design processes of 4P APIs.
     - Significant contributions to different phases of the API lifecycle.
@@ -88,17 +100,28 @@ export const ui = {
 
     'experience.title': 'Experiencia Profesional',
 
+    'experience.nfq.title': 'SCIB Integration - Proyecto Financial Crime - Banco Santander',
+    'experience.nfq.date': 'Octubre de 2024 - Actualmente',
+    'experience.nfq.text': `- Actualización y mantenimiento de servicios.
+    - Documentación técnica de procesos.
+    - Extracción de evidencias sobre cambios e implementaciones en el software.
+    - Desarrollo de herramientas de automatización de tareas.
+    - Gestión de colecciones y testing de APIs en Postman.
+    - Gestión de Bases de Datos.
+    - Implementación de protocolos de seguridad.
+    - Expedición, firma, verificación y gestión de certificados digitales.`,
+
     'experience.uxcale.title': 'API Engineer en uXcale',
-    'experience.uxcale.date': 'Septiembre de 2022 - Actualmente',
+    'experience.uxcale.date': 'Septiembre de 2022 - Octubre de 2024',
     'experience.uxcale.text': `- Desarrollo de programa formativo de introducción a las APIs.
     - Diseño y especificación de APIs de carácter interno destinadas a la gestión de empleados y actividades.
     - Documentación acerca de numerosos conceptos API, así como de numerosas tecnologías, como OpenAPI, Apigee, Mulesoft, gRPC, AsyncAPI, GraphQL entre otras.
     - Implementación de APIs utilizando diferentes entornos (SpringBoot, NodeJS...).
     - Realización de pruebas y testing con Jest, Supertest, Junit 5 y Mockito.
-    - Utilización de clientes API como Postman, Advanced Rest Client o Insomnia`,
+    - Utilización de clientes API como Postman, Advanced Rest Client o Insomnia.`,
     
     'experience.telefonica.title': 'API OB Integration Engineer en Telefónica',
-    'experience.telefonica.date': 'Enero de 2023 - Actualmente',
+    'experience.telefonica.date': 'Enero de 2023 - Octubre de 2024',
     'experience.telefonica.text': `- Integración de proyectos entre Telefónica Kernel Global y la OB de Brasil y equipos de Vivo.
     - Implicación en los procesos de análisis y diseño de las APIs de 4P.​
     - Aportación de mejoras significativas en las diferentes fases del ciclo de vida de las APIs.​
