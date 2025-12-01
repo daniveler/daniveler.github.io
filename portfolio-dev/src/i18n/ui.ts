3c2e33faf0b2efab3f9f60e29b4bc37001153d3f
daniveler <daniel01velerdas@gmail.com>
2025-12-01 17:51:59 +0100

    Update experience with nter

--- portfolio-dev/src/i18n/ui.ts
+++ portfolio-dev/src/i18n/ui.ts
@@ -23,8 +23,20 @@ export const ui = {
     'social-media.title': 'Social Media',
 
     'experience.title': 'Professional Experience',
+
+    'experience.nter.title': 'Financial Crime & Fraud Prevention Consultant - Banco Santander',
+    'experience.nter.date': 'October 2024 - Present',
+    'experience.nter.text': `- Workflows planification and management using Control-M, starting from public and private lists loading to generation of suspected fraud alerts.
+    - Monitorization of fraud detection system with Oracle’s suite: FCCM, EDQ, IPE, OBIEE, Weblogic, Oracle SQL Developer, etc., ensuring availability, data integrity and proper functioning of the alert generation system .
+    - Formating and validation of clients and payments files according to SEPA (ISO20022), SWIFT MT/MX and FEDWIRE standards, ensuring their proper processing.
+    - Development of tools and automations using Python and Bash, resulting in productivity increase on repetitive tasks.
+    - Management and testing of Postman's microservices APIs collections, ensuring successful integration and proper functioning of services.
+    - Preparation of technical documentation and evidence extraction of changes and implementations, contributing to internal regulatory compliance and enabling trazability of deployments.
+    - Security protocols implementation, including creation, signing, and verification of digital certificates in order to estabilsh secure connections between services, ensuring regulatory compliance and integrity in comunications.`,
+
+
     'experience.uxcale.title': 'API Engineer in uXcale',
-    'experience.uxcale.date': 'September 2022 - Present',
+    'experience.uxcale.date': 'September 2022 - October 2024',
     'experience.uxcale.text': `- Development of an introductory training program for APIs.
     - Design and specification of internal APIs for employee and activity management.
     - Documentation on numerous API concepts and various technologies, such as OpenAPI, Apigee, Mulesoft, gRPC, AsyncAPI, and GraphQL, among others.
@@ -33,7 +45,7 @@ export const ui = {
     - Utilization of API clients such as Postman, Advanced Rest Client, or Insomnia.`,
 
     'experience.telefonica.title': 'API OB Integration Engineer in Telefónica',
-    'experience.telefonica.date': 'January 2023 - Present',
+    'experience.telefonica.date': 'January 2023 - October 2024',
     'experience.telefonica.text': `- Project integration between Telefónica Kernel Global and Vivo Brazil team.
     - Involvement in the analysis and design processes of 4P APIs.
     - Significant contributions to different phases of the API lifecycle.
@@ -88,8 +100,18 @@ export const ui = {
 
     'experience.title': 'Experiencia Profesional',
 
+    'experience.nter.title': 'Consultor en Financial Crime & Fraud Prevention - Banco Santander',
+    'experience.nter.date': 'Octubre 2024 - Actualmente',
+    'experience.nter.text': `- Planificación y gestión de workflows con Control-M, desde la carga de listas públicas y privadas hasta la generación de alertas sospechosas de fraude.
+    - Monitorización del sistema de detección de fraude mediante la suite de Oracle: FCCM, EDQ, IPE, OBIEE, Weblogic, Oracle SQL Developer, etc., asegurando la disponibilidad, integridad de datos y el correcto funcionamiento de las alertas.
+    - Formateo y validación de ficheros de clientes y pagos conforme a estándares SEPA (ISO20022), SWIFT MT/MX y FEDWIRE, asegurando su correcto procesamiento en el sistema.
+    - Desarrollo de herramientas y automatizaciones con Python y Bash, resultando en un aumento de la productividad en tareas repetitivas.
+    - Gestión y testing de colecciones de APIs de microservicios en Postman, asegurando la correcta integración y funcionamiento de los servicios.
+    - Elaboración de documentación técnica y extracción de evidencias de cambios e implementaciones, contribuyendo al cumplimiento de la normativa interna y facilitando la trazabilidad de los despliegues.
+    - Implementación de protocolos de seguridad, incluyendo la expedición, firma y verificación de certificados digitales para establecer conexiones seguras entre servicios, asegurando el cumplimiento normativo y la integridad de las comunicaciones.`,
+
     'experience.uxcale.title': 'API Engineer en uXcale',
-    'experience.uxcale.date': 'Septiembre de 2022 - Actualmente',
+    'experience.uxcale.date': 'Septiembre de 2022 - Octubre de 2024',
     'experience.uxcale.text': `- Desarrollo de programa formativo de introducción a las APIs.
     - Diseño y especificación de APIs de carácter interno destinadas a la gestión de empleados y actividades.
     - Documentación acerca de numerosos conceptos API, así como de numerosas tecnologías, como OpenAPI, Apigee, Mulesoft, gRPC, AsyncAPI, GraphQL entre otras.
@@ -98,7 +120,7 @@ export const ui = {
     - Utilización de clientes API como Postman, Advanced Rest Client o Insomnia`,
     
     'experience.telefonica.title': 'API OB Integration Engineer en Telefónica',
-    'experience.telefonica.date': 'Enero de 2023 - Actualmente',
+    'experience.telefonica.date': 'Enero de 2023 - Octubre de 2024',
     'experience.telefonica.text': `- Integración de proyectos entre Telefónica Kernel Global y la OB de Brasil y equipos de Vivo.
     - Implicación en los procesos de análisis y diseño de las APIs de 4P.​
     - Aportación de mejoras significativas en las diferentes fases del ciclo de vida de las APIs.​
